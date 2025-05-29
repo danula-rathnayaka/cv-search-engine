14f32455c0cdcfab386ac1670fdd052df595cc81
Danula <danusathmin@gmail.com>
2025-05-30 02:16:26 +0530

    init: create frontend base structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-neutral-950 text-white">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
